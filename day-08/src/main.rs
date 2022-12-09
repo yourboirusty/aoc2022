fc1f421b10e3941e9bd78bb4ff0d5ab53a6030bf
Kornel Koszela <kornel.koszela@sofomo.com>
2022-12-09 16:25:42 +0100

    Day8.2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -55,6 +55,47 @@ fn visible_sides(tree: (usize, usize), forest: &Vec<Vec<u8>>) -> u8 {
 
     visible_sides
 }
+
+fn get_scenic_score(tree: (usize, usize), forest: &Vec<Vec<u8>>) -> u32 {
+    let tree_value = forest[tree.0][tree.1];
+    let mut view_distance: Vec<u32> = Vec::new();
+
+    let edges: Vec<(Vec<usize>, Vec<usize>)> = vec![
+        ((0..tree.0).rev().collect(), vec![tree.1]),      //up
+        ((tree.0..forest.len()).collect(), vec![tree.1]), //down
+        (vec![tree.0], (tree.1..forest.len()).collect()), //right
+        (vec![tree.0], (0..tree.1).rev().collect()),      //left
+    ];
+
+    for edge in edges {
+        let mut visible_sides: u32 = 0;
+        let mut view_blocked = false;
+        for row_idx in edge.0.clone() {
+            for col_idx in edge.1.clone() {
+                if (row_idx, col_idx) == tree {
+                    continue;
+                }
+                visible_sides += 1;
+                let other_tree = forest[row_idx][col_idx];
+                if tree_value <= other_tree {
+                    view_blocked = true;
+                    break;
+                }
+            }
+            if view_blocked {
+                break;
+            };
+        }
+        view_distance.push(visible_sides);
+    }
+
+    let mut scenic_score: u32 = 1;
+    for distance in view_distance {
+        scenic_score *= distance;
+    }
+    scenic_score
+}
+
 fn look_at_trees(forest: &Vec<Vec<u8>>) -> u32 {
     let mut trees_visible = (forest.len() * 2 + (forest[0].len() - 2) * 2) as u32;
 
@@ -66,7 +107,21 @@ fn look_at_trees(forest: &Vec<Vec<u8>>) -> u32 {
     trees_visible
 }
 
+fn assess_the_trees(forest: &Vec<Vec<u8>>) -> u32 {
+    let mut scenic_score = 0;
+    for row_idx in 1..forest.len() - 1 {
+        for col_idx in 1..forest[row_idx].len() - 1 {
+            let new_scenic_score = get_scenic_score((row_idx, col_idx), forest);
+            if new_scenic_score > scenic_score {
+                scenic_score = new_scenic_score;
+            }
+        }
+    }
+    scenic_score
+}
+
 fn main() {
     let forest = load_data("./data/input");
     println!("{:?}", look_at_trees(&forest));
+    println!("{:?}", assess_the_trees(&forest));
 }
